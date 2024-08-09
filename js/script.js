a34fc51594bccde51eed044a5fe3fdd1090be2bd
hanion77 <speciic77@naver.com>
2024-08-09 17:47:24 +0900

    scroll Animation complete

--- js/script.js
+++ js/script.js
@@ -10,14 +10,20 @@ let partnerListCount = document.querySelectorAll('.partner_list li').length;
 let partnerListLeft = 0;
 let partnerListTotalWidth = partnerListWidth * partnerListCount;
 let animation;
+let bookATrip = document.querySelector('.book_a_trip');
+let bookATripOST = bookATrip.offsetTop - 300;
+let progressBar = document.querySelector('.progress_bar');
+let bar = document.querySelector('.progress_bar .bar');
+let onGoingPercent = document.querySelector('.ongoing .percent');
 
 partnerList.style.width = partnerListTotalWidth+'px';
 
 function movePartnerList(){
+  //partnerListLeft = partnerListLeft - 5;
   partnerListLeft -= 2;
   if(partnerListLeft === -partnerListTotalWidth/2){
     partnerListLeft = 0;
-  };
+  }
   partnerList.style.left = partnerListLeft+'px';
   animation = requestAnimationFrame(movePartnerList);
 }
@@ -25,39 +31,31 @@ requestAnimationFrame(movePartnerList);
 
 partnerList.addEventListener('mouseenter',()=>{
   cancelAnimationFrame(animation)
-})
+});
 partnerList.addEventListener('mouseleave',()=>{
-  requestAnimationFrame(movePartnerList)
-})
+  requestAnimationFrame(movePartnerList);
+});
 
-/*
-pagers를 클릭하면 할일
-  모든 a에서 active를 제거하고, 클릭한 그 요소에 active 추가  
-*/
+
 pagers.forEach((item,idx)=>{
-  item.addEventListener('click', (e)=>{
+  item.addEventListener('click',(e)=>{
     e.preventDefault();
-
-
     showTestimonial(idx);
   });
 });
-
 function showTestimonial(num){
   /*
-  첫 후기인데, -1 마지막 후기가 보이도록
-  마지막 후기인데, 3, 첫 후기가 보이도록
-  
-  if(num === -1){
-    num == testimonialCount -1;
-  }
-  if(num === 3){
-    num = 0
-  }
-
+    if(num === -1){
+      num = testimonialCount -1;
+    } else if (num === 3){
+      num = 0
+    } else{
+      num = num; 
+    }
+ 
   num = (num === -1) ? testimonialCount -1 : (num === 3) ? 0: num;
-  */
-  num = (num + testimonialCount) % testimonialCount;
+   */
+  num = (num + testimonialCount) % testimonialCount; 
   /*
   num = (1+3) % 3 = 1
   num1 num1
@@ -82,9 +80,32 @@ function showTestimonial(num){
 
 }
 
-paginationDown.addEventListener('click', ()=>{
-  showTestimonial(currentIdx + 1)
+paginationDown.addEventListener('click',()=>{
+  showTestimonial(currentIdx + 1);
 });
-paginationUp.addEventListener('click', ()=>{
-  showTestimonial(currentIdx - 1)
+paginationUp.addEventListener('click',()=>{
+  showTestimonial(currentIdx - 1);
 });
+
+window.addEventListener('scroll',()=>{
+  let scrollAmt = window.scrollY;
+  if(scrollAmt >= bookATripOST){
+    if(!bookATrip.classList.contains('active')){
+      bookATrip.classList.add('active');
+      onGoingNumAnimation();
+    }
+  }  
+});
+
+function onGoingNumAnimation(){
+  let targetNum = Number(bar.getAttribute('data-rate'));
+  let num = 0;
+  let animation = setInterval(()=>{
+    num += 1;
+    bar.style.width = num+'%';
+    onGoingPercent.innerHTML = num+'%';
+    if(num === targetNum){
+      clearInterval(animation);
+    }
+  },50);
+}
